7cfae76492db7ae72b8c1110e021cec3091f121c
Brennan Brown <brennan.brown@edu.sait.ca>
2020-08-21 22:46:03 -0600

    Change deployment from Heroku to AWS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") or b"\x1c=\xb2\xfa?\xbcn\x91K\x9c\xe7=\x8c\xa5i\xff"
+SECRET_KEY = b"\x1c=\xb2\xfa?\xbcn\x91K\x9c\xe7=\x8c\xa5i\xff"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-brennan.herokuapp.com", "localhost"]
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",
+                 "django-brennan-dev.us-west-2.elasticbeanstalk.com"]
 
 # Application definition
 
@@ -39,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs'
+    'jobs',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -72,6 +71,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
+# AWS S3 Configuration
+# URLs MUST follow this template:
+# https://BUCKETNAME.s3.REGIONNAME.amazonaws.com/folder/file
+
+AWS_STORAGE_BUCKET_NAME = "django-portfolio-brennan" or os.environ.get(
+    "AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "us-west-2" or os.environ.get("AWS_S3_REGION_NAME")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
+    AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+AWS_DEFAULT_ACL = None
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'portfolio.custom_storages.StaticStorage'
+
+MEDIAFILES_LOCATION = 'media'
+DEAFULT_FILE_STORAGE = 'portfolio.custom_storages.MediaStorage'
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
@@ -79,14 +103,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("ENV_NAME") or 'portfoliodb',
-        'USER': os.environ.get("ENV_USER") or 'postgres',
-        'PASSWORD': os.environ.get("ENV_PASSWORD") or 'password123456',
-        'HOST': os.environ.get("ENV_HOST") or 'localhost',
-        'PORT': os.environ.get("ENV_PORT") or '5432',
+        'NAME': os.environ['RDS_DB_NAME'] or 'portfoliodb',
+        'USER': os.environ['RDS_USERNAME'] or 'postgres',
+        'PASSWORD': os.environ['RDS_PASSWORD'] or 'password123456',
+        'HOST': os.environ['RDS_HOSTNAME'] or 'localhost',
+        'PORT': os.environ['RDS_PORT'] or '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -124,15 +147,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
-MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
-
-if (os.environ.get("ENV_NAME") != None):
-    # Activate Django-Heroku.
-    django_heroku.settings(locals())
-
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+MEDIA_ROOT = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
