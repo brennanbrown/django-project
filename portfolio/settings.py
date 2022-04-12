dec3569da61229d9310d9ef5f166b79dad42ab45
Brennan Brown <mail@brennanbrown.ca>
2022-04-11 23:43:08 -0600

    Fix ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY") or b"\x1c=\xb2\xfa?\xbcn\x91K\x9c\xe7=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["brennan-django.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["brennan-django.herokuapp.com", "localhost"]
 
 # Application definition
 
