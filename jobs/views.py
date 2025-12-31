551993f1d8ab964c4f8ec53ab2e6f908ff556ca8
Brennan Kenneth Brown <brennankbrown+education@gmail.com>
2025-12-31 09:46:04 -0400

    Add database error handling to views

--- jobs/views.py
+++ jobs/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.db import OperationalError
 
 from .models import Job
 
 # Create your views here.
 def home(request):
-    jobs = Job.objects.all()
-    return render(request, 'jobs/home.html', {'jobs': jobs})
+    try:
+        jobs = Job.objects.all()
+        return render(request, 'jobs/home.html', {'jobs': jobs})
+    except OperationalError:
+        # Handle case where database tables don't exist
+        return render(request, 'jobs/home.html', {'jobs': []})
 
 def detail(request, job_id):
-    job_detail = get_object_or_404(Job, pk=job_id)
-    return render(request, 'jobs/detail.html', {'job':job_detail})
+    try:
+        job_detail = get_object_or_404(Job, pk=job_id)
+        return render(request, 'jobs/detail.html', {'job':job_detail})
+    except OperationalError:
+        # Handle case where database tables don't exist
+        return render(request, 'jobs/detail.html', {'job': None})
